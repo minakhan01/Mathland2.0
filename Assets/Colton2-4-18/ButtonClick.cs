c2195afaf270b77a438e0622a8bed06357e451dc
Colton Stearns <cstearns@mit.edu>
2018-02-04 13:03:31 -0500

    Game State Manager
    Created game state manager that initialzes gravity

---
+++ Assets/Colton2-4-18/ButtonClick.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonClick : MonoBehaviour {
+
+	public GameObject playText;
+	public GameObject buildText;
+
+	// Use this for initialization
+	void Start () {
+		Button btn = gameObject.GetComponent<Button>();
+		btn.onClick.AddListener(TaskOnClick);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void TaskOnClick () {
+		Game_State_Manager.Game_state = !Game_State_Manager.Game_state;
+		if (Game_State_Manager.Game_state) {
+			playText.SetActive (false);
+			buildText.SetActive (true);
+		}
+		else {
+			playText.SetActive (true);
+			buildText.SetActive (false);
+		}
+	}
+
+}
+
+
