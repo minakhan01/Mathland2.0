82da3c0f03fbb789dcfbdd395f2b473e41578aad
stacia <edinastacia@gmail.com>
2018-02-03 22:03:33 -0500

    Add Select, Drag, and Resize scripts (DragObject, PinchScale, SelectObject)

---
+++ Assets/SelectObject.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectObject : MonoBehaviour {
+
+	void OnMouseDown()
+	{
+		PinchScale.ScaleTransform = this.transform;
+	}
+}
