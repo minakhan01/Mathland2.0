3c5c986f62e47cfa9c75fa1c3902aed24f6399cc
stacia <edinastacia@gmail.com>
2018-02-25 17:45:00 -0500

    touch scene

--- Assets/TouchInputManager.cs
+++ Assets/TouchInputManager.cs
@@ -4,6 +4,21 @@ using UnityEngine;
 
 public class TouchInputManager : MonoBehaviour {
 
+	const float pinchTurnRatio = Mathf.PI / 2;
+	const float minTurnAngle = 0;
+
+	const float pinchRatio = 1;
+	const float minPinchDistance = 0;
+
+	const float panRatio = 1;
+	const float minPanDistance = 0;
+
+	static public float turnAngleDelta;
+	static public float turnAngle;
+
+	static public float pinchDistanceDelta;
+	static public float pinchDistance;
+
 	public enum TouchState {Stationary, Repositioning, Resizing, Rotating, Deleting};
 	public TouchState CurrentTouchState;
 	private float HoldTime = 5;
@@ -15,6 +30,8 @@ public class TouchInputManager : MonoBehaviour {
 	public float twistScreenToWorldRatio = 5.0f;
 	public float rotateMinLimit = 0;
 	public float rotateMaxLimit = 80;
+	private float DraggingTreshold=0.7f;
+	private float LongPressTreshold=3f;
 	float PreviousFingerDistance, zoomDistance, distWeight;
 
 	// Use this for initialization
@@ -22,104 +39,118 @@ public class TouchInputManager : MonoBehaviour {
 		CurrentTouchState = TouchState.Stationary;
 	}
 
+	static private float Angle (Vector2 pos1, Vector2 pos2) {
+		Vector2 from = pos2 - pos1;
+		Vector2 to = new Vector2(1, 0);
+
+		float result = Vector2.Angle( from, to );
+		Vector3 cross = Vector3.Cross( from, to );
+
+		if (cross.z > 0) {
+			result = 360f - result;
+		}
+
+		return result;
+	}
+
+	void draggingDetected() {
+		CurrentTouchState = TouchState.Repositioning;
+		Debug.Log ("Drag Object");
+		// ----- UniversalInputManager.updatePosition (-touchDeltaPosition.x * TouchSpeed, -touchDeltaPosition.y * TouchSpeed);
+	}
+
+	void rotatingDetected() {
+		CurrentTouchState = TouchState.Rotating;
+		Debug.Log ("Rotate Object");
+		// ------ UniversalInputManager.RotateObject (FingerAngleDelta);
+	}
+
+	void pinchingDetected() {
+		CurrentTouchState = TouchState.Resizing;
+		Debug.Log ("Resize Object");
+		// ------ UniversalInputManager.ResizeObject (FingerDistanceDelta);
+	}
+
+	void deletingDetected() {
+		CurrentTouchState = TouchState.Deleting;
+		Debug.Log ("Delete Object");
+		// ----- UniversalInputManager.DeleteObject ();
+	}
+
+	void stationaryDetected() {
+		AccumulateTime = 0;
+		CurrentTouchState = TouchState.Stationary;
+		Debug.Log ("Stationary Object");
+	}
+
 	// Update is called once per frame
 	void Update () {
-		if (Input.touchCount == 1) {
-
-			if (Input.GetTouch (0).phase == TouchPhase.Began) {
-				AccumulateTime = 0;
-			} else if (Input.GetTouch (0).phase == TouchPhase.Stationary) {
-				AccumulateTime += Input.GetTouch (0).deltaTime;
-				if(AccumulateTime > 3) {
-					CurrentTouchState = TouchState.Deleting;
-					Debug.Log ("Delete Object");
-					// ----- UniversalInputManager.DeleteObject ();
-				}
-			} else if (Input.GetTouch (0).phase == TouchPhase.Moved) {
-				Vector2 touchDeltaPosition = Input.GetTouch (0).deltaPosition;
-				if (touchDeltaPosition.magnitude > 0.5f) {
-					// Move object across XY plane
-					CurrentTouchState = TouchState.Repositioning;
-					Debug.Log ("Drag Object");
-					// ----- UniversalInputManager.updatePosition (-touchDeltaPosition.x * TouchSpeed, -touchDeltaPosition.y * TouchSpeed);
-				}
-			}
-			else if (Input.GetTouch (0).phase == TouchPhase.Ended) {
-				AccumulateTime = 0;
-				CurrentTouchState = TouchState.Stationary;
-			}
-		}
-		else if (Input.touchCount == 2) {
-			// Codes found at https://answers.unity.com/questions/205342/gesture-rotation-clamp.html
-			// with modification
-
-			// fingers data
-			Touch Finger_0 = Input.GetTouch(0);
-			Touch Finger_1 = Input.GetTouch(1);
-
-			// fingers positions
-			Vector3 Finger_0_Position = new Vector3(Finger_0.position.x, Finger_0.position.y, 0);
-			Vector3 Finger_1_Position = new Vector3(Finger_1.position.x, Finger_1.position.y, 0);
-
-			// vector of fingers movements
-			Vector3 Finger_0_Delta = new Vector3(Finger_0.deltaPosition.x, Finger_0.deltaPosition.y, 0);
-			Vector3 Finger_1_Delta = new Vector3(Finger_1.deltaPosition.x, Finger_1.deltaPosition.y, 0);
-
-			// fingers distance
-			float FingerDistance = Vector3.Distance(Finger_0.position, Finger_1.position);
-
-			// if both fingers moving
-			if (Finger_0.phase == TouchPhase.Moved && Finger_1.phase == TouchPhase.Moved) {
-
-				// fingers moving direction
-				Vector3 Finger_0_Direction = Finger_0_Delta.normalized;
-				Vector3 Finger_1_Direction = Finger_1_Delta.normalized;
-
-				// dot product of directions
-				float dot = Vector3.Dot(Finger_0_Direction, Finger_1_Direction);
-
-				// if fingers moving in opposite directions
-				if (dot < -0.7f) {
-
-					float FingerDistanceDelta = FingerDistance - PreviousFingerDistance;
-
-					/* for resizing, the direction of the finger must be opposite of each other, so the
-					 sum of the magnitude = 0 */
-					if (Mathf.Abs(Finger_0_Direction.magnitude + Finger_1_Direction.magnitude) < 1) {
-						CurrentTouchState = TouchState.Resizing;
-						Debug.Log ("Resize Object");
-						// ------ UniversalInputManager.ResizeObject (FingerDistanceDelta);
-					}
-					else if (Finger_0_Delta.magnitude > 2 && Finger_1_Delta.magnitude > 2) {
-						// detect twist (Rotate object in XY plane)
-						CurrentTouchState = TouchState.Rotating;
-						Debug.Log ("Rotate Object in XY Plane");
-
-                     	Vector3 fingersDir = (Finger_1_Position - Finger_0_Position).normalized;
-                     	Vector3 twistNormal = Vector3.Cross(fingersDir, Vector3.forward);
-                     	Vector3 twistAxis = Vector3.Cross(fingersDir, twistNormal);
-                     	float averageDelta = (Finger_0_Delta.magnitude + Finger_1_Delta.magnitude) / 2;
-
-						// -------- UniversalInputManager.RotateObjectXY ();
-                 	}
-					else if(Mathf.Abs(Finger_0_Direction.magnitude - Finger_1_Direction.magnitude) < 1){
-						// detect rotate object in yz plane
-						/* for rotating, the direction of the finger must be the same, so the
-							 difference of the magnitude = 0 */
-						CurrentTouchState = TouchState.Rotating;
-						Debug.Log("Rotate Object in YZ Plane");
-						// --------- UniversalInputManager.RotateObjectYZ (FingerDistanceDelta);
-					}
-				}
-			}
-
-			// record last distance, for delta distances
-			PreviousFingerDistance = FingerDistance;
+		if (Input.touchCount > 0) {
 
 			if (Input.GetTouch (0).phase == TouchPhase.Ended) {
-				CurrentTouchState = TouchState.Stationary;
+				stationaryDetected();
 			}
-		}
+			else if (Input.touchCount == 1) {
 
+				if (Input.GetTouch (0).phase == TouchPhase.Began) {
+					AccumulateTime = 0;
+				} else if (Input.GetTouch (0).phase == TouchPhase.Stationary) {
+					AccumulateTime += Input.GetTouch (0).deltaTime;
+					if(AccumulateTime > LongPressTreshold) {
+						deletingDetected();
+					}
+				} else if (Input.GetTouch (0).phase == TouchPhase.Moved) {
+					Vector2 touchDeltaPosition = Input.GetTouch (0).deltaPosition;
+					if (touchDeltaPosition.magnitude > DraggingTreshold) {
+						// Move object across XY plane
+						draggingDetected();
+					}
+				}
+				else if (Input.GetTouch (0).phase == TouchPhase.Ended) {
+					stationaryDetected();
+				}
+			}
+			else if (Input.touchCount == 2) {
+
+				//pinchDistance = pinchDistanceDelta = 0;
+				//turnAngle = turnAngleDelta = 0;
+
+				Touch touch1 = Input.GetTouch(0);
+				Touch touch2 = Input.GetTouch(1);
+
+				// ... if at least one of them moved ...
+				if (touch1.phase == TouchPhase.Moved || touch2.phase == TouchPhase.Moved) {
+					// ... check the delta distance between them ...
+					pinchDistance = Vector2.Distance(touch1.position, touch2.position);
+					float prevDistance = Vector2.Distance(touch1.position - touch1.deltaPosition,
+						touch2.position - touch2.deltaPosition);
+					pinchDistanceDelta = pinchDistance - prevDistance;
+
+					// ... if it's greater than a minimum threshold, it's a pinch!
+					if (Mathf.Abs(pinchDistanceDelta) > minPinchDistance) {
+						pinchDistanceDelta *= pinchRatio;
+						Debug.Log ("Pinch Distance" + pinchDistanceDelta);
+						pinchingDetected();
+					} else {
+						pinchDistance = pinchDistanceDelta = 0;
+					}
+
+					// ... or check the delta angle between them ...
+					turnAngle = Angle(touch1.position, touch2.position);
+					float prevTurn = Angle(touch1.position - touch1.deltaPosition,
+						touch2.position - touch2.deltaPosition);
+					turnAngleDelta = Mathf.DeltaAngle(prevTurn, turnAngle);
+
+					// ... if it's greater than a minimum threshold, it's a turn!
+					if (Mathf.Abs(turnAngleDelta) > minTurnAngle) {
+						turnAngleDelta *= pinchTurnRatio;
+						Debug.Log ("Angle Delta" + turnAngleDelta);
+						rotatingDetected();
+					} else {
+						turnAngle = turnAngleDelta = 0;
+					}
+				}
+			}  
+		}
 	}
 }
