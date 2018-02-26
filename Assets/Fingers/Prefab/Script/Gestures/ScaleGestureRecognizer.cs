b70ac55fe2dcdc9bea64f727422cebff83b25cd1
Mina Khan <minkhn01>
2018-02-26 09:28:30 -0500

    push assts: fingers, gui, and mesh rotator

---
+++ Assets/Fingers/Prefab/Script/Gestures/ScaleGestureRecognizer.cs
@@ -0,0 +1,175 @@
+﻿//
+// Fingers Gestures
+// (c) 2015 Digital Ruby, LLC
+// http://www.digitalruby.com
+// Source code may be used for personal or commercial projects.
+// Source code may NOT be redistributed or sold.
+// 
+
+using System;
+
+namespace DigitalRubyShared
+{
+    /// <summary>
+    /// A scale gesture detects two fingers moving towards or away from each other to scale something
+    /// </summary>
+    public class ScaleGestureRecognizer : GestureRecognizer
+    {
+        private const float minimumScaleResolutionSquared = 1.005f;
+        private const float stationaryScaleResolutionSquared = 1.05f;
+        private const float stationaryTimeSeconds = 0.1f; // if stationary for this long, use stationaryScaleResolutionSquared else minimumScaleResolutionSquared
+        private const float hysteresisScaleResolutionSquared = 1.1f; // higher values resist scaling in the opposite direction more
+
+        private float previousDistance;
+        private float previousDistanceDirection;
+        private float previousDistanceX;
+        private float previousDistanceY;
+        private float initialDistance;
+        private float initialDistanceX;
+        private float initialDistanceY;
+
+        private readonly System.Diagnostics.Stopwatch timer = new System.Diagnostics.Stopwatch();
+
+        public ScaleGestureRecognizer()
+        {
+            ScaleMultiplier = ScaleMultiplierX = ScaleMultiplierY = 1.0f;
+            ZoomSpeed = 3.0f;
+            ThresholdUnits = 0.15f;
+            MinimumNumberOfTouchesToTrack = MaximumNumberOfTouchesToTrack = 2;
+            timer.Start();
+        }
+
+        private void UpdateCenter(float distance, float distanceX, float distanceY)
+        {
+            previousDistance = distance;
+            previousDistanceX = distanceX;
+            previousDistanceY = distanceY;
+        }
+
+        private void ProcessTouches()
+        {
+            CalculateFocus(CurrentTrackedTouches);
+
+            if (!TrackedTouchCountIsWithinRange)
+            {
+                return;
+            }
+
+            float distance = DistanceBetweenPoints(CurrentTrackedTouches[0].X, CurrentTrackedTouches[0].Y, CurrentTrackedTouches[1].X, CurrentTrackedTouches[1].Y);
+            float distanceX = Distance(CurrentTrackedTouches[0].X - CurrentTrackedTouches[1].X);
+            float distanceY = Distance(CurrentTrackedTouches[0].Y - CurrentTrackedTouches[1].Y);
+
+            if (State == GestureRecognizerState.Possible)
+            {
+                if (previousDistance == 0.0f)
+                {
+                    previousDistance = distance;
+                    previousDistanceX = distanceX;
+                    previousDistanceY = distanceY;
+                }
+                else
+                {
+                    float diff = Math.Abs(previousDistance - distance);
+                    if (diff >= ThresholdUnits)
+                    {
+                        initialDistance = distance;
+                        initialDistanceX = distanceX;
+                        initialDistanceY = distanceY;
+                        UpdateCenter(distance, distanceX, distanceY);
+                        SetState(GestureRecognizerState.Began);
+                    }
+                }
+            }
+            else if (State == GestureRecognizerState.Executing)
+            {
+                if (distance != previousDistance)
+                {
+                    float jitterThreshold = (float)timer.Elapsed.TotalSeconds <= stationaryTimeSeconds ? minimumScaleResolutionSquared : stationaryScaleResolutionSquared;
+                    float currentDistanceSquared = distance * distance;
+                    float previousDistanceSquared = previousDistance * previousDistance;
+                    if ((currentDistanceSquared - previousDistanceSquared) * previousDistanceDirection < 0.0f)
+                    {
+                        jitterThreshold = Math.Max(jitterThreshold, hysteresisScaleResolutionSquared);
+                    }
+                    bool aboveJitterThreshold = ((previousDistanceSquared > jitterThreshold * currentDistanceSquared) ||
+                        (currentDistanceSquared > jitterThreshold * previousDistanceSquared));
+                    if (aboveJitterThreshold)
+                    {
+                        timer.Reset();
+                        timer.Start();
+                        ScaleMultiplier = Math.Max(0.1f, 1.0f + (ZoomSpeed * ((distance - previousDistance) / initialDistance)));
+                        ScaleMultiplierX = Math.Max(0.1f, 1.0f + (ZoomSpeed * ((distanceX - previousDistanceX) / initialDistanceX)));
+                        ScaleMultiplierY = Math.Max(0.1f, 1.0f + (ZoomSpeed * ((distanceY - previousDistanceY) / initialDistanceY)));
+                        previousDistance = distance;
+                        previousDistanceX = distanceX;
+                        previousDistanceY = distanceY;
+                        previousDistanceDirection = Math.Sign(currentDistanceSquared - previousDistanceSquared);
+                        SetState(GestureRecognizerState.Executing);
+                    }
+                }
+            }
+            else if (State == GestureRecognizerState.Began)
+            {
+                SetState(GestureRecognizerState.Executing);
+            }
+            else
+            {
+                SetState(GestureRecognizerState.Possible);
+            }
+        }
+
+        protected override void TouchesBegan(System.Collections.Generic.IEnumerable<GestureTouch> touches)
+        {
+            previousDistance = 0.0f;
+        }
+
+        protected override void TouchesMoved()
+        {
+            ProcessTouches();
+        }
+
+        protected override void TouchesEnded()
+        {
+            if (State == GestureRecognizerState.Executing)
+            {
+                CalculateFocus(CurrentTrackedTouches);
+                SetState(GestureRecognizerState.Ended);
+            }
+            else
+            {
+                // didn't get to the executing state, fail the gesture
+                SetState(GestureRecognizerState.Failed);
+            }
+        }
+
+        /// <summary>
+        /// The current scale multiplier. Multiply your current scale value by this to scale.
+        /// </summary>
+        /// <value>The scale multiplier.</value>
+        public float ScaleMultiplier { get; private set; }
+
+        /// <summary>
+        /// The current scale multiplier for x axis. Multiply your current scale x value by this to scale.
+        /// </summary>
+        /// <value>The scale multiplier.</value>
+        public float ScaleMultiplierX { get; private set; }
+
+        /// <summary>
+        /// The current scale multiplier for y axis. Multiply your current scale y value by this to scale.
+        /// </summary>
+        /// <value>The scale multiplier.</value>
+        public float ScaleMultiplierY { get; private set; }
+
+        /// <summary>
+        /// Additional multiplier for ScaleMultipliers. This will making scaling happen slower or faster. Default is 3.0.
+        /// </summary>
+        /// <value>The zoom speed.</value>
+        public float ZoomSpeed { get; set; }
+
+        /// <summary>
+        /// How many units the distance between the fingers must increase or decrease from the start distance to begin executing. Default is 0.15.
+        /// </summary>
+        /// <value>The threshold in units</value>
+        public float ThresholdUnits { get; set; }
+    }
+}
---
+++ Assets/TouchTest.cs
@@ -0,0 +1,237 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DigitalRubyShared;
+
+public class TouchTest : MonoBehaviour {
+
+	private TapGestureRecognizer tapGesture;
+	private TapGestureRecognizer doubleTapGesture;
+	private TapGestureRecognizer tripleTapGesture;
+	private SwipeGestureRecognizer swipeGesture;
+	private PanGestureRecognizer panGesture;
+	private ScaleGestureRecognizer scaleGesture;
+	private RotateGestureRecognizer rotateGesture;
+	private LongPressGestureRecognizer longPressGesture;
+
+	private float nextAsteroid = float.MinValue;
+	private GameObject draggingAsteroid;
+
+	private readonly List<Vector3> swipeLines = new List<Vector3>();
+
+	private void DebugText(string text, params object[] format)
+	{
+		//bottomLabel.text = string.Format(text, format);
+		Debug.Log(string.Format(text, format));
+	}
+		
+
+	private void HandleSwipe(float endX, float endY)
+	{
+		Debug.Log ("HandleSwipe");
+	}
+
+	private void TapGestureCallback(GestureRecognizer gesture)
+	{
+		if (gesture.State == GestureRecognizerState.Ended)
+		{
+			Debug.Log ("TapGesture");
+		}
+	}
+
+	private void CreateTapGesture()
+	{
+		tapGesture = new TapGestureRecognizer();
+		tapGesture.StateUpdated += TapGestureCallback;
+		tapGesture.RequireGestureRecognizerToFail = doubleTapGesture;
+		FingersScript.Instance.AddGesture(tapGesture);
+	}
+
+	private void DoubleTapGestureCallback(GestureRecognizer gesture)
+	{
+		if (gesture.State == GestureRecognizerState.Ended)
+		{
+			Debug.Log ("DoubleTapEnded");
+		}
+	}
+
+	private void CreateDoubleTapGesture()
+	{
+		doubleTapGesture = new TapGestureRecognizer();
+		doubleTapGesture.NumberOfTapsRequired = 2;
+		doubleTapGesture.StateUpdated += DoubleTapGestureCallback;
+		doubleTapGesture.RequireGestureRecognizerToFail = tripleTapGesture;
+		FingersScript.Instance.AddGesture(doubleTapGesture);
+	}
+
+	private void SwipeGestureCallback(GestureRecognizer gesture)
+	{
+		if (gesture.State == GestureRecognizerState.Ended)
+		{
+			Debug.Log ("SwipeGestureCallback");
+		}
+	}
+
+	private void CreateSwipeGesture()
+	{
+		swipeGesture = new SwipeGestureRecognizer();
+		swipeGesture.Direction = SwipeGestureRecognizerDirection.Any;
+		swipeGesture.StateUpdated += SwipeGestureCallback;
+		swipeGesture.DirectionThreshold = 1.0f; // allow a swipe, regardless of slope
+		FingersScript.Instance.AddGesture(swipeGesture);
+	}
+
+	private void PanGestureCallback(GestureRecognizer gesture)
+	{
+		if (gesture.State == GestureRecognizerState.Executing)
+		{
+			DebugText("Panned, Location: {0}, {1}, Delta: {2}, {3}", gesture.FocusX, gesture.FocusY, gesture.DeltaX, gesture.DeltaY);
+
+			float deltaX = panGesture.DeltaX / 25.0f;
+			float deltaY = panGesture.DeltaY / 25.0f;
+			Debug.Log ("PanGestureCallback");
+		}
+	}
+
+	private void CreatePanGesture()
+	{
+		panGesture = new PanGestureRecognizer();
+		panGesture.MinimumNumberOfTouchesToTrack = 2;
+		panGesture.StateUpdated += PanGestureCallback;
+		FingersScript.Instance.AddGesture(panGesture);
+	}
+
+	private void ScaleGestureCallback(GestureRecognizer gesture)
+	{
+		Debug.Log ("ScaleGestureCallback");
+	}
+
+	private void CreateScaleGesture()
+	{
+		scaleGesture = new ScaleGestureRecognizer();
+		scaleGesture.StateUpdated += ScaleGestureCallback;
+		FingersScript.Instance.AddGesture(scaleGesture);
+	}
+
+	private void RotateGestureCallback(GestureRecognizer gesture)
+	{
+		if (gesture.State == GestureRecognizerState.Executing)
+		{
+			Debug.Log ("RotateGestureCallback began" + rotateGesture.RotationRadiansDelta * Mathf.Rad2Deg);
+		}
+	}
+
+	private void CreateRotateGesture()
+	{
+		rotateGesture = new RotateGestureRecognizer();
+		rotateGesture.StateUpdated += RotateGestureCallback;
+		FingersScript.Instance.AddGesture(rotateGesture);
+	}
+
+	private void LongPressGestureCallback(GestureRecognizer gesture)
+	{
+		if (gesture.State == GestureRecognizerState.Began)
+		{
+			Debug.Log ("LongPressGestureCallback hello began");
+		}
+		else if (gesture.State == GestureRecognizerState.Executing)
+		{
+			Debug.Log ("LongPressGestureCallback hello executing");
+		}
+		else if (gesture.State == GestureRecognizerState.Ended)
+		{
+			Debug.Log ("LongPressGestureCallback hello ended");
+		}
+	}
+
+	private void CreateLongPressGesture()
+	{
+		longPressGesture = new LongPressGestureRecognizer();
+		longPressGesture.MaximumNumberOfTouchesToTrack = 1;
+		longPressGesture.StateUpdated += LongPressGestureCallback;
+		FingersScript.Instance.AddGesture(longPressGesture);
+	}
+
+	private void PlatformSpecificViewTapUpdated(GestureRecognizer gesture)
+	{
+		if (gesture.State == GestureRecognizerState.Ended)
+		{
+			Debug.Log("You triple tapped the platform specific label!");
+		}
+	}
+
+	private void CreatePlatformSpecificViewTripleTapGesture()
+	{
+		tripleTapGesture = new TapGestureRecognizer();
+		tripleTapGesture.StateUpdated += PlatformSpecificViewTapUpdated;
+		tripleTapGesture.NumberOfTapsRequired = 3;
+		FingersScript.Instance.AddGesture(tripleTapGesture);
+	}
+
+	private static bool? CaptureGestureHandler(GameObject obj)
+	{
+		// I've named objects PassThrough* if the gesture should pass through and NoPass* if the gesture should be gobbled up, everything else gets default behavior
+		if (obj.name.StartsWith("PassThrough"))
+		{
+			// allow the pass through for any element named "PassThrough*"
+			return false;
+		}
+		else if (obj.name.StartsWith("NoPass"))
+		{
+			// prevent the gesture from passing through, this is done on some of the buttons and the bottom text so that only
+			// the triple tap gesture can tap on it
+			return true;
+		}
+
+		// fall-back to default behavior for anything else
+		return null;
+	}
+
+	// Use this for initialization
+	void Start () {
+		// don't reorder the creation of these :)
+		CreatePlatformSpecificViewTripleTapGesture();
+		CreateDoubleTapGesture();
+		CreateTapGesture();
+		CreateSwipeGesture();
+		CreatePanGesture();
+		CreateScaleGesture();
+		CreateRotateGesture();
+		CreateLongPressGesture();
+
+		// pan, scale and rotate can all happen simultaneously
+//		panGesture.AllowSimultaneousExecution(scaleGesture);
+//		panGesture.AllowSimultaneousExecution(rotateGesture);
+//		scaleGesture.AllowSimultaneousExecution(rotateGesture);
+		scaleGesture.DisallowSimultaneousExecution(rotateGesture);
+		// prevent the one special no-pass button from passing through,
+		//  even though the parent scroll view allows pass through (see FingerScript.PassThroughObjects)
+		FingersScript.Instance.CaptureGestureHandler = CaptureGestureHandler;
+
+		// show touches, only do this for debugging as it can interfere with other canvases
+		FingersScript.Instance.ShowTouches = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	private void LateUpdate()
+	{
+		
+		int touchCount = Input.touchCount;
+		if (FingersScript.Instance.TreatMousePointerAsFinger && Input.mousePresent)
+		{
+			touchCount += (Input.GetMouseButton(0) ? 1 : 0);
+			touchCount += (Input.GetMouseButton(1) ? 1 : 0);
+			touchCount += (Input.GetMouseButton(2) ? 1 : 0);
+		}
+		string touchIds = string.Empty;
+		foreach (Touch t in Input.touches)
+		{
+			touchIds += ":" + t.fingerId + ":";
+		}
+
+	}
+}
