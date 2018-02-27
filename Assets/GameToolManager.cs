eaff9ea223949c1b80c27d7e65a8a6821edb57e0
Mina Khan <minkhn01>
2018-02-27 12:35:49 -0500

    create GameToolManager

---
+++ Assets/GameToolManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using HoloToolkit.Unity;
+
+public class GameToolManager : Singleton<GameToolManager> {
+
+	public void createForceField() {
+		// TODO: Ashris
+	}
+
+	public void CreateVelocityVector() {
+	}
+
+	public void CreateRope() {
+	}
+
+	public void CreateCube() {
+	}
+
+	public void CreateRamp() {
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
