535de80fe353ba43d010479f7afed5ad353a4b28
Ashris Choudhury <ashris.me@gmail.com>
2018-02-28 17:12:21 -0500

    Made fixes

--- Assets/GameToolManager.cs
+++ Assets/GameToolManager.cs
@@ -6,7 +6,7 @@ using HoloToolkit.Unity;
 public class GameToolManager : Singleton<GameToolManager> {
 
 	public GameObject ForceField, VelocityVector, Rope, Cube, Ramp;
-	private List<GameObject> gameToolList;
+	private List<GameObject> gameToolList = new List<GameObject> ();
 
 	public void CreateForceField() {
 		CreateGameToolPrefab (ForceField);
@@ -30,8 +30,10 @@ public class GameToolManager : Singleton<GameToolManager> {
 	}
 
 	private void CreateGameToolPrefab(GameObject prefab){
-		GameObject gb=Instantiate (prefab, new Vector3(1,0,5),Quaternion.identity);
+		GameObject gb=Instantiate (prefab, new Vector3(1,0,5),Quaternion.identity) as GameObject;
 		gameToolList.Add (gb);
+		Debug.Log ("The list now is");
+		Debug.Log(gameToolList);
 	}
 	public void DestroyGameTool(GameObject gb){
 		GameObject.Destroy (gb);
@@ -40,14 +42,14 @@ public class GameToolManager : Singleton<GameToolManager> {
 
 	public void DestroyAllGameTools(){
 		foreach (GameObject gb in gameToolList) {
-			GameObject.Destroy (gb);
 			gameToolList.Remove (gb);
+			GameObject.Destroy (gb);
 		}
 	}
 
 	// Use this for initialization
 	void Start () {
-		
+		Debug.Log ("The script started");
 	}
 	
 	// Update is called once per frame
