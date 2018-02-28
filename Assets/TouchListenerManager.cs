cc1752563667d1138fc4d0d5d67fd45be44480bd
Mina Khan <minkhn01>
2018-02-28 15:55:57 -0500

    dragging abilities

--- Assets/TouchListenerManager.cs
+++ Assets/TouchListenerManager.cs
@@ -20,6 +20,11 @@ public class TouchListenerManager : Singleton<GameStateManager> {
 			return false;
 	}
 
+	private bool objectDragging()
+	{
+		return (TouchInputManager.CurrentTouchState == TouchInputManager.TouchState.Repositioning && TouchInputManager.selectedObject != null);
+	}
+
 	private bool longPressOutsideDetected()
 	{
 		if (TouchInputManager.selectedObject == null && TouchInputManager.CurrentTouchState == TouchInputManager.TouchState.longPress)
@@ -35,10 +40,34 @@ public class TouchListenerManager : Singleton<GameStateManager> {
 			Debug.Log ("change to modify screen");
 			GameStateManager.switchDisplayState ();
 		} 
+		else if (GameStateManager.currentDisplayState == GameStateManager.GameDisplayState.MODIFY_SCREEN && objectDragging())
+		{
+			TouchInputManager.selectedObject.transform.position = new Vector3(Input.touches[0].position.x, Input.touches[0].position.y, 1);
+		}
 		else if (GameStateManager.currentDisplayState == GameStateManager.GameDisplayState.MODIFY_SCREEN && longPressOutsideDetected ()) 
 		{
 			Debug.Log ("change to play screen");
 			GameStateManager.switchDisplayState ();
 		}
+
+
+//		if (Input.touchCount == 1) {
+//			Touch touch = Input.GetTouch(0);
+//			Vector3 initialPosition = rb.position;
+//			bool drag = false;
+//			// if user touch the object = drag
+//			if (touch.phase == TouchPhase.Began) {
+//				initialPosition = rb.position;
+//				// if the touch is on the object = that means user want to drag the object
+//				if (Vector2.Distance (Input.touches[0].position, new Vector2(initialPosition.x, initialPosition.y)) < 0.05) {
+//					drag = true;
+//				}
+//			} else if (drag == true) {
+//				Vector2 currentPosition = Input.touches[0].position;
+//				//				GameStateFunctions.moveObject(currentPosition);
+//			}
+//		}
 	}
+
+
 }
