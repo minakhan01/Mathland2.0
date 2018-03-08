846adb2039755706456c449eafbd8cbf5ad8eeaf
Colton Stearns <cstearns@mit.edu>
2018-03-08 15:36:16 -0500

    Added BallMotionManager script

---
+++ Assets/BallMotionManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallMotionManager : MonoBehaviour {
+
+    private Vector3 ballNetForceExperienced;
+    private Vector3 ballCurrentVelocity;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
