f222ff9c9ae1ed42c5e1e368edb8d40f695a1cdc
Ashris Choudhury <ashris.me@gmail.com>
2018-03-09 17:03:33 -0500

    make edits to velocitytrigger

--- Assets/VelocityTrigger.cs
+++ Assets/VelocityTrigger.cs
@@ -18,25 +18,16 @@ public class VelocityTrigger : MonoBehaviour {
 
     private void OnTriggerEnter(Collider collidee)
     {
-        if (collidee.gameObject.GetComponent<VelocityReactor>() != null)
-        {
-            BALL = collidee.gameObject;
             gameObject.GetComponent<MeshRenderer>().material.color = Color.green;
             //GameObject.Find("AudioManager").GetComponent<Martana>().Sayit("Velocity Vector Active");
 			VelocityResponse.Instance.addVelocityVector(gameObject);
 			Debug.Log ("VelocityVector and ball collided!");
-        }
     }
     private void OnTriggerExit(Collider collidee)
     {
-        if (collidee.gameObject.GetComponent<VelocityReactor>() != null)
-        {
-            //BALL = null;
             gameObject.GetComponent<MeshRenderer>().material.color = Color.white;
 			VelocityResponse.Instance.removeVelocityVector(gameObject);
 			Debug.Log ("VelocityVector and ball collision removed");
-            //GameObject.Find("AudioManager").GetComponent<Martana>().Sayit("Velocity Vector Inactive");
-        }
     }
     private void OnDestroy()
     {
