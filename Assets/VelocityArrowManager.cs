e56c585db9dbe366107f0e6830cbb9eb13c71974
stacia <edinastacia@gmail.com>
2018-03-13 16:55:27 -0400

    change tail and head to kinematic

--- Assets/VelocityArrowManager.cs
+++ Assets/VelocityArrowManager.cs
@@ -41,6 +41,6 @@ public class VelocityArrowManager : Singleton<VelocityArrowManager> {
 	{
 		//Quaternion toRotation = Quaternion.FromToRotation(transform.up, BallPhysicsManager.Instance.updatedVelocity);
 		//transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(BallPhysicsManager.Instance.updatedVelocity), 1.0f * Time.time);
-		transform.rotation = Quaternion.LookRotation (new Vector3(BallPhysicsManager.Instance.updatedVelocity.x, BallPhysicsManager.Instance.updatedVelocity.y, BallPhysicsManager.Instance.updatedVelocity.z));
+		transform.rotation = Quaternion.LookRotation (new Vector3(BallPhysicsManager.Instance.updatedVelocity.z, BallPhysicsManager.Instance.updatedVelocity.y, BallPhysicsManager.Instance.updatedVelocity.x));
 	}
 }
