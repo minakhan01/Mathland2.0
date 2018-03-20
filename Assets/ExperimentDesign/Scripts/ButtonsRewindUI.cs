ec0e336a5d875d3512f1ea637ea2b084798772e4
jsirera <judsirera@gmail.com>
2018-03-20 12:28:51 -0400

    Rewind UI logic

---
+++ Assets/ExperimentDesign/Scripts/ButtonsRewindUI.cs
@@ -0,0 +1,97 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonsRewindUI : MonoBehaviour
+{
+
+    [Header("Sprites")]
+    public Sprite pauseButton;
+    public Sprite playButton;
+
+    [Header("ButtonsProperties")]
+    public ButtonProperties playPauseButton;
+    public ButtonProperties replayButton;
+    public ButtonProperties speedButton;
+
+
+    public GameObject speedMenu;
+    public Text speedMenuText;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void playPauseButtonHandler()
+    {
+        playPauseButton.switchState();
+        if (RewindManager.Instance.currentPlayMode == RewindManager.PlayMode.PLAY)
+        {
+            RewindManager.Instance.currentPlayMode = RewindManager.PlayMode.PAUSE;
+            RewindManager.Instance.pause();
+        }
+        else
+        {
+            RewindManager.Instance.currentPlayMode = RewindManager.PlayMode.PLAY;
+            RewindManager.Instance.play();
+        }
+    }
+
+    public void replayButtonHandler()
+    {
+        RewindManager.Instance.replay();
+    }
+
+    public void speedButtonHandler()
+    {
+        speedMenu.GetComponent<ButtonProperties>().switchState();
+        speedMenu.SetActive(true);
+    }
+
+    public void doubleSlowSpeedButtonHandler()
+    {
+        RewindManager.Instance.currentSpeed = RewindManager.Instance.PlaySpeed[0];
+        updateSpeedButton();
+    }
+
+    public void quarterSlowSpeedButtonHandler()
+    {
+        RewindManager.Instance.currentSpeed = RewindManager.Instance.PlaySpeed[1];
+        updateSpeedButton();
+    }
+
+    public void normalSpeedButtonHandler()
+    {
+        RewindManager.Instance.currentSpeed = RewindManager.Instance.PlaySpeed[2];
+        updateSpeedButton();
+    }
+
+    public void doubleFastSpeedButtonHandler()
+    {
+        RewindManager.Instance.currentSpeed = RewindManager.Instance.PlaySpeed[3];
+        updateSpeedButton();
+    }
+
+    public void fourTimesFastSpeedButtonHandler()
+    {
+        RewindManager.Instance.currentSpeed = RewindManager.Instance.PlaySpeed[4];
+        updateSpeedButton();
+    }
+
+    void updateSpeedButton () {
+        speedMenu.SetActive(false);
+        speedMenuText.text = "x" + RewindManager.Instance.currentSpeed.ToString();
+    }
+
+
+}
---
+++ Assets/ExperimentDesign/Scripts/RewindManager.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using HoloToolkit.Unity;
+
+public class RewindManager : Singleton<RewindManager>
+{
+
+    public enum RewindMode { ON, OFF };
+    public enum PlayMode { PLAY, PAUSE };
+    public readonly float[] PlaySpeed = {0.5f, 0.25f, 1f, 2f, 4f};
+
+    public float currentSpeed { get; set; }
+    public PlayMode currentPlayMode { get; set; }
+    public float currentTime { get; set; }
+
+    [Header("Rewind Objects")]
+    public GameObject rewindUI;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        currentTime = 0f;
+        currentSpeed = 1f;
+        rewindUI.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void replay()
+    {
+        currentTime = 0f;
+    }
+
+    public void play()
+    {
+        currentPlayMode = PlayMode.PLAY;
+    }
+
+    public void pause()
+    {
+        currentPlayMode = PlayMode.PAUSE;
+    }
+
+    public void setRewindUIActiveAgain()
+    {
+        rewindUI.SetActive(true);
+    }
+
+}
