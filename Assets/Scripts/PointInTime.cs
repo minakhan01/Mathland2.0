798202e6d903b32c4aabb11159caef618acbc1fd
Ashris Choudhury <ashris.me@gmail.com>
2018-03-20 15:09:55 -0400

    Fresh push

---
+++ Assets/Scripts/PointInTime.cs
@@ -0,0 +1,32 @@
+﻿
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PointInTime : MonoBehaviour {
+
+	public Quaternion rotation;
+	public Vector3 scale,velocity,force,position;
+	// Use this for initialization
+
+
+	public PointInTime(Vector3 _position,Quaternion _rotation,Vector3 _scale,Vector3 _velocity,Vector3 _force){
+		position = _position;
+		rotation = _rotation;
+		scale = _scale;
+		velocity = _velocity;
+		force = _force;
+
+	}
+
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
