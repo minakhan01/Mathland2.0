8f1c06caa29e2b29b01bca323a976cbb5ca2c621
Mina Khan <minkhn01>
2018-03-25 15:06:24 -0400

    scene 8 done

---
+++ Assets/Scene8Initializer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scene8Initializer : MonoBehaviour {
+
+	public GameObject ForceField, VelocityArrow;
+	public float ForceMagnitude = -1.0f;
+	public float VelocityMagnitude = 3.0f;
+
+	// Use this for initialization
+	void Start () {
+		ForceMagnitude *= 0.001f;
+		VelocityMagnitude *= 0.001f;
+		GameObject DirectionArrow;
+		DirectionArrow = ForceField.transform.Find ("Direction").gameObject;
+		DirectionArrow.transform.localScale = new Vector3(DirectionArrow.transform.localScale.x, ForceMagnitude, DirectionArrow.transform.localScale.z);
+		VelocityArrow.transform.localScale = new Vector3 (VelocityMagnitude, VelocityMagnitude, VelocityMagnitude);
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
