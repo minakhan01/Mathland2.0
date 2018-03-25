ed26ef9ccb3ad708d6ff218c044497277fc2f145
Mina Khan <minkhn01>
2018-03-25 08:08:53 -0400

    scene 2 setup

---
+++ Assets/Scene2Initializer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scene2Initializer : MonoBehaviour {
+
+	public GameObject VelocityArrow1, VelocityArrow2;
+	public float Velocity1Magnitude = 1.0f;
+	public float Velocity2Magnitude = 1.5f;
+
+	// Use this for initialization
+	void Start () {
+		Velocity1Magnitude *= 0.002f;
+		Velocity2Magnitude *= 0.002f;
+		VelocityArrow1.transform.localScale = new Vector3 (Velocity1Magnitude, Velocity1Magnitude, Velocity1Magnitude);
+		VelocityArrow2.transform.localScale = new Vector3 (Velocity2Magnitude, Velocity2Magnitude, Velocity2Magnitude);
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
