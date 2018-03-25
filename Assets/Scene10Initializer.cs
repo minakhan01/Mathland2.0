d4030e926214b939d5c0dbbc8183bb9ef4d1daff
Mina Khan <minkhn01>
2018-03-25 16:24:12 -0400

    real ball working

---
+++ Assets/Scene10Initializer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scene10Initializer : MonoBehaviour {
+
+	public GameObject VelocityArrow;
+	public float VelocityMagnitude = 5.0f;
+
+	// Use this for initialization
+	void Start () {
+		VelocityMagnitude *= 0.001f;
+		VelocityArrow.transform.localScale = new Vector3 (VelocityMagnitude, VelocityMagnitude, VelocityMagnitude);
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
